c280aea3baae4d652588e79a8b8f50089f569c56
WittyKyrie <649547021@qq.com>
2024-07-06 19:48:33 +0800

    保存数据

--- Assets/ScriptableObjects/FoodData/FoodData.cs
+++ Assets/ScriptableObjects/FoodData/FoodData.cs
@@ -1,21 +1,63 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Battle
 {
+    public enum FoodName
+    {
+        Beef,
+        Biscuit, //饼干
+        Bread,
+        Broccoli, //西兰花
+        Cake,
+        Chicken,
+        Egg,
+        Fish,
+        Mushroom,
+        Noodle,
+        Prawn,
+        Rice,
+        Stone,
+        Tomato,
+        WatermelonSlice,
+        Shrimp,
+        HotDog,
+        Sausage,
+
+        [LabelText("咖喱饭")] RiceAndBeef, //咖喱饭
+        [LabelText("蛋炒饭")] RiceAndEgg, //蛋炒饭
+        [LabelText("虾饺")] ShrimpAndBiscuit, //虾饺
+        [LabelText("西瓜拌饭")] WatermelonAndRice, //西瓜拌饭
+        [LabelText("蛋饺")] EggAndBiscuit, //蛋饺
+        [LabelText("热狗")] SausageAndBread,
+        [LabelText("海鲜面")] ShrimpAndNoodle,
+        [LabelText("牛角面包")] BeefAndBread,
+        [LabelText("番茄鱼")] TomatoAndFish,
+        [LabelText("三明治")] BreadAndChicken,
+        [LabelText("饺子")] ChickenAndBiscuit,
+    }
+
+    [Serializable]
+    public class FoodFormula
+    {
+        [LabelText("可以合成的食物")] public FoodName combineFood;
+        [LabelText("最终生成的食物")] public FoodName finalFood;
+    }
+
     [Serializable]
     [CreateAssetMenu(fileName = "FoodData", menuName = "Data/FoodData", order = 1)]
     public class FoodData : SerializedScriptableObject
     {
+        public FoodName foodName;
         public int foodScore;
         [ShowInInspector] public CellsInfo<bool> GridConfig;
         [PreviewField] public Sprite stage1;
-        [PreviewField] public Sprite stage2;
         [PreviewField] public Sprite plateSprite;
         [SerializeField] public float Phase0Time;
         [SerializeField] public float Phase1Time;
+
+        public List<FoodFormula> foodFormulas;
     }
 }
