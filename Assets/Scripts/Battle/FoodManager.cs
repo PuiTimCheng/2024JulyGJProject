dda77c8e2a8156a183939fb0ee406fbfbfe8a1a6
PuiTimCheng <chengpuitim@163.com>
2024-07-07 14:32:38 +0800

    stop game

--- Assets/Scripts/Battle/FoodManager.cs
+++ Assets/Scripts/Battle/FoodManager.cs
@@ -48,7 +48,11 @@ namespace Battle
                 OnMouseUp();
             }
         }
-        
+
+        public void ClearFood()
+        {
+            transform.DestroyChildren();
+        }
         void OnMouseHold()
         {
             if (StomachManager.Instance.CurrentSelecting != null &&
